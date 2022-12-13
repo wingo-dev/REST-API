91656138c8a46dbb00a9f1998a8c019607292ef1
= <rukshanlancer@gmail.com>
2022-12-13 11:15:21 +0800

    REST api fetch all articles

--- app.js
+++ app.js
@@ -15,13 +15,23 @@ app.use(express.static("public"));
 
 mongoose.connect("mongodb://localhost:27017/wikiDB", { useNewUrlParser: true });
 // mongoose.set('strictQuery', true);
-const articles = {
+const articleSchema = {
     title: String,
     content: String
 }
+
+const Article = mongoose.model("Article", articleSchema);
 // todo
+app.get("/articles", function (req, res) {
+    Article.find(function (err, foundArticles) {
+        if (!err) {
+            res.send(foundArticles);
+        } else {
+            res.send(err);
+        }
+    });
+});
 
 app.listen(3000, function () {
     console.log("Server started on port 3000");
-
 });
